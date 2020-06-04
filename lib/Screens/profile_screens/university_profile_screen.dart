f6c58d7dbb2e90603f936a165900e34836610ce2
Tudor Batica <tudor.batica@gmail.com>
2020-06-05 00:01:01 +0300

    updated the uni profile screen

--- lib/Screens/profile_screens/university_profile_screen.dart
+++ lib/Screens/profile_screens/university_profile_screen.dart
@@ -1,15 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:professor_review/models/faculty_summary_data.dart';
 import 'package:professor_review/screens/profile_screens/faculty_profile_screen.dart';
-import 'package:professor_review/models/information_list_tile_data.dart';
-import 'package:professor_review/models/preview_list_tile_data.dart';
 import 'package:professor_review/models/university.dart';
 import 'package:professor_review/services/database_service.dart';
-import 'package:professor_review/widgets/entity_screen_header.dart';
-import 'package:professor_review/widgets/information_list_tile.dart';
 import 'package:professor_review/widgets/loading.dart';
-import 'package:professor_review/widgets/preview_list_tile.dart';
+import 'package:professor_review/widgets/profile_screen_header.dart';
+import 'package:professor_review/widgets/title_and_rating_list_tile.dart';
+import 'package:professor_review/widgets/two_weights_box.dart';
 import 'package:provider/provider.dart';
 
 class UniversityProfileScreen extends StatelessWidget {
@@ -17,91 +14,92 @@ class UniversityProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var _university = Provider.of<University>(context);
 
-    // the information for the info tiles of this screen
-    var informationDataList = <InformationListTileData>[
-      InformationListTileData(
-          icon: Icons.location_on,
-          text: _university.city + ", " + _university.country),
-      InformationListTileData(
-          icon: Icons.school,
-          text: _university.faculties.length != 1
-              ? _university.faculties.length.toString() + " faculties"
-              : _university.faculties.length.toString() + " faculty")
-    ];
-
-    return _university == null
-        ? Loading()
-        : Scaffold(
-            backgroundColor: Theme.of(context).primaryColor,
-            appBar: AppBar(
-                backgroundColor: Theme.of(context).primaryColor,
-                elevation: 0.0),
-            body: Column(
-              children: <Widget>[
-                Center(
-                  child: EntityScreenHeader(
-                      averageRating: _university.averageRating,
-                      averageRatingTitle: "Average rating",
-                      entityName: _university.name),
-                ),
-                SizedBox(height: 25),
-                Expanded(
-                  child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(40.0),
-                            topRight: Radius.circular(40.0)),
-                      ),
-                      child: ListView.builder(
-                          itemCount: _university.faculties.length +
-                              informationDataList.length,
-                          itemBuilder: (context, index) => index <
-                                  informationDataList.length
-                              ? InformationListTile(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.1,
-                                  dividerLength:
-                                      MediaQuery.of(context).size.width *
-                                          (1 / pow(2, index + 1)),
-                                  information: informationDataList[index],
-                                )
-                              : GestureDetector(
-                                  onTap: () => Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => StreamProvider(
-                                              create: (_) => DatabaseService
-                                                  .instance
-                                                  .faculty(_university
-                                                      .faculties[index -
-                                                          informationDataList
-                                                              .length]
-                                                      .facultyReference),
-                                              child: FacultyProfileScreen()))),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(15.0),
-                                    child: PreviewListTile(
-                                      data: PreviewListTileData(
-                                          data: [
-                                            InformationListTileData(
-                                                icon: Icons.location_city,
-                                                text: _university
-                                                    .faculties[index -
-                                                        informationDataList
-                                                            .length]
-                                                    .facultyName),
-                                          ],
-                                          rating: _university
-                                              .faculties[index -
-                                                  informationDataList.length]
-                                              .averageRating),
-                                    ),
-                                  ),
-                                ))),
-                )
-              ],
+    return _university == null ? Loading() : Scaffold(
+       backgroundColor: Theme.of(context).primaryColorDark,
+            body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // info header
+                  _profileScreenHeader(context, _university),
+                  // faculties section
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: Text("Faculties",
+                        style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w800,
+                            color: Theme.of(context).primaryColorLight)),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  // faculties row
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: _facultiesScrollView(context, _university.faculties),
+                  ),
+                  SizedBox(height: 20.0)
+                ],
+              ),
             ),
-          );
+    );
+  }
+
+   Widget _profileScreenHeader(context, University university) {
+    return ProfileScreenHeader(
+      image: Image.asset('images/university.jpg'),
+      information: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            university.name,
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w800,
+                color: Theme.of(context).primaryColorDark),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          Text(
+            "${university.city}, ${university.country}",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).primaryColorDark),
+          ),
+        ],
+      ),
+      bottomInformation: TwoWeightsBox(
+          boldedText: university.averageRating.toString(), normalText: "Rating"),
+    );
+  }
+
+   Widget _facultiesScrollView(context, faculties) {
+    // build the list of professors
+    List<Widget> facultyTiles = [];
+    for (FacultySummaryData faculty in faculties) {
+      // add the professor
+      facultyTiles.add(GestureDetector(
+        onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => StreamProvider(
+                      create: (_) => DatabaseService.instance
+                          .faculty(faculty.facultyReference),
+                      child: FacultyProfileScreen(),
+                    ))),
+        child: TitleAndRatingListTile(
+          image: Image.asset('images/faculty.png'),
+          rating: faculty.averageRating.toString(),
+          title: faculty.facultyName,
+        ),
+      ));
+      // add some spacing
+      facultyTiles.add(SizedBox(width: 20.0));
+    }
+    // return a scrollable row with the list of reviews
+    return SingleChildScrollView(
+        scrollDirection: Axis.vertical, child: Row(children: facultyTiles));
   }
 }
